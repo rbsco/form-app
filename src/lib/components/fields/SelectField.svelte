19ee61e4aa5af64d9e66d9878556e65ddb910e56
Timothy Elems <elemstimothy@protonmail.com>
2026-02-18 03:24:41 +0000

    SelectField debugs

--- src/lib/components/fields/SelectField.svelte
+++ src/lib/components/fields/SelectField.svelte
@@ -84,7 +84,7 @@
 
   $: hasError = error && error.length > 0;
   $: selectClasses = [
-    'form-field',
+    'form-field w-full px-4 py-2.5 rounded-lg border bg-opacity-80 bg-white focus:outline-none transition-all',
     hasError ? 'error-field' : '',
     isFocused ? 'ring-2 ring-primary' : '',
     disabled ? 'opacity-50 cursor-not-allowed' : ''
@@ -110,15 +110,6 @@
     bind:value
     {disabled}
     class={selectClasses}
-    class:w-full
-    class:px-4
-    class:py-2.5
-    class:rounded-lg
-    class:border
-    class:bg-opacity-80
-    class:bg-white
-    class:focus:outline-none
-    class:transition-all
     on:change={handleChange}
     on:focus={handleFocus}
     on:blur={handleBlur}
