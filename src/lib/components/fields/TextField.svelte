dd111f5dffa94465d78d36cefb19e104bcd7faa4
Timothy Elems <elemstimothy@protonmail.com>
2026-02-18 03:10:59 +0000

    Dynamic form debug

--- src/lib/components/fields/TextField.svelte
+++ src/lib/components/fields/TextField.svelte
@@ -124,7 +124,8 @@
     class:py-2.5
     class:rounded-lg
     class:border
-    class:bg-white/80
+    class:bg-opacity-80
+    class:bg-white
     class:focus:outline-none
     class:transition-all
     on:input={handleInput}
